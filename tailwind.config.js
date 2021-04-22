792aa899515b07504c08577825aa9d91e88ef0cd
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2021-04-22 19:23:28 +0430

    update daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,6 @@ module.exports = {
     // list of your markup files
     './index.html'
   ],
-  theme: {
-    extend: {
-      colors: require('daisyui/colors')
-    },
-  },
   daisyui: {
     styled: true, // set false if you only need the skeleton of components
     themes: true, // set false if you dont need DaisyUI themes
